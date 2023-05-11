3e405025fba197d496ef129bad4fbce7df3b983b
AleksandrNikiforov <aleksandr040988@gmail.com>
2023-05-11 22:35:43 +0500

    add logout correct login html

--- presalesite/registration/urls.py
+++ presalesite/registration/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, re_path
 import re
 from .views import register_view, login_view
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', register_view, name='Registration'),
     path('login/', login_view, name='Login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='Login'), name='Logout'),
 ]
