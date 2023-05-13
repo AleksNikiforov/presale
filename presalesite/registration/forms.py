563ae15a858f7cf564f41f19c680a441fc1c9077
AleksandrNikiforov <aleksandr040988@gmail.com>
2023-05-13 15:37:36 +0500

    correct register form

--- presalesite/registration/forms.py
+++ presalesite/registration/forms.py
@@ -10,3 +10,26 @@ class MyLoginForm(AuthenticationForm):
         self.fields['username'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Username'})
         self.fields['password'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Password'})
 
+
+class CustomUserCreationForm(UserCreationForm):
+    password1 = forms.CharField(
+        strip=False,
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password', 'title': 'Enter your password, password can’t be too similar to your other personal information, must contain at least 8 characters, can’t be entirely numeric' }),
+        help_text='',
+    )
+
+    password2 = forms.CharField(
+        strip=False,
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Confirm password', 'title': 'Repeat your password'}),
+        help_text='',
+    )
+    
+    username = forms.CharField(
+        label='Username',
+        max_length=150,
+        help_text='',
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username', 'title': 'Enter your username'}),
+    )
+    class Meta:
+        model = User
+        fields = ['username', 'password1', 'password2']
--- presalesite/registration/views.py
+++ presalesite/registration/views.py
@@ -7,13 +7,13 @@ from .forms import *
 
 def register_view(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
             return redirect('Rates')
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     return render(request, 'registration/register.html', {'form': form})
 
 
